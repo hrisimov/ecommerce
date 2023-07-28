dc80e4f4196f5b5c91c3f5f13cd8546b21589971
hrisimov <140465371+hrisimov@users.noreply.github.com>
2023-07-28 16:13:40 +0300

    Added custom user model and manager

--- ecommerce/ecommerce/accounts/models.py
+++ ecommerce/ecommerce/accounts/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth import models as auth_models
+from django.contrib.auth import validators as auth_validators
+from django.core import validators
 from django.db import models
 
-# Create your models here.
+from ecommerce.accounts.managers import EcommerceUserManager
+
+
+class EcommerceUser(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
+    USERNAME_MAX_LENGTH = 50
+
+    email = models.EmailField(
+        unique=True,
+        validators=(validators.EmailValidator(),),
+    )
+
+    username = models.CharField(
+        max_length=USERNAME_MAX_LENGTH,
+        unique=True,
+        validators=(auth_validators.UnicodeUsernameValidator(),),
+    )
+
+    is_staff = models.BooleanField(
+        default=False,
+    )
+
+    is_active = models.BooleanField(
+        default=False,
+    )
+
+    date_joined = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    objects = EcommerceUserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username',)
