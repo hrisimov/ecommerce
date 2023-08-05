4faf3dc608d1b9634b0151953f8931d0b2ab9231
hrisimov <140465371+hrisimov@users.noreply.github.com>
2023-08-05 19:08:55 +0300

    Added user and profile models to admin

--- ecommerce/ecommerce/accounts/admin.py
+++ ecommerce/ecommerce/accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from ecommerce.accounts.models import Profile
+
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'username', 'is_staff', 'is_active')
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'first_name', 'last_name')
