62dd548c69a15b1b21b4624cc402b4eecc7e853f
hrisimov <140465371+hrisimov@users.noreply.github.com>
2023-08-06 16:24:49 +0300

    Added 'AuditEntity' abstract model and 'Category' model

--- ecommerce/ecommerce/products/models.py
+++ ecommerce/ecommerce/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from ecommerce.common.abstract_models import AuditEntity
+
+
+class Category(AuditEntity):
+    NAME_MAX_LENGTH = 50
+
+    name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+        unique=True,
+    )
+
+    slug = models.SlugField(
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
