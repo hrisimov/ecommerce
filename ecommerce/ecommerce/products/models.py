eedca351d9f55451ae0b56b406bdc8145b934e36
hrisimov <140465371+hrisimov@users.noreply.github.com>
2023-08-06 16:36:25 +0300

    Added 'Product' model

--- ecommerce/ecommerce/products/models.py
+++ ecommerce/ecommerce/products/models.py
@@ -20,3 +20,39 @@ class Category(AuditEntity):
 
     class Meta:
         verbose_name_plural = 'Categories'
+
+
+class Product(AuditEntity):
+    NAME_MAX_LENGTH = 50
+
+    PRICE_MAX_DIGITS = 6
+    PRICE_DECIMAL_PLACES = 2
+
+    name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+        unique=True,
+    )
+
+    description = models.TextField()
+
+    price = models.DecimalField(
+        max_digits=PRICE_MAX_DIGITS,
+        decimal_places=PRICE_DECIMAL_PLACES,
+    )
+
+    stock = models.PositiveIntegerField()
+
+    slug = models.SlugField(
+        unique=True,
+    )
+
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.RESTRICT,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('-created_on',)
