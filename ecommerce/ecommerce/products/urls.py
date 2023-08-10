16ca187fb8bcc56099ec84c719b1f8d028db2ee3
hrisimov <140465371+hrisimov@users.noreply.github.com>
2023-08-10 23:24:59 +0300

    Added products list view and media files configuration

--- ecommerce/ecommerce/products/urls.py
+++ ecommerce/ecommerce/products/urls.py
@@ -1,3 +1,10 @@
+from django.urls import path
+
+from ecommerce.products.views import ProductsListView
+
 app_name = 'products'
 
-urlpatterns = ()
+urlpatterns = (
+    path('', ProductsListView.as_view(), name='list'),
+    path('category/<slug:slug>/', ProductsListView.as_view(), name='list by category'),
+)
--- ecommerce/ecommerce/products/views.py
+++ ecommerce/ecommerce/products/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views import generic as views
 
-# Create your views here.
+from ecommerce.products.models import Product, Category
+
+
+class ProductsListView(views.ListView):
+    model = Product
+    template_name = 'products/products_list.html'
+    paginate_by = 12
+    by_category = None
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        category_slug = self.kwargs.get('slug')
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            queryset = queryset.filter(category=category)
+            self.by_category = category
+
+        return queryset.prefetch_related('productimage_set')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['category'] = self.by_category
+        context['categories'] = Category.objects.all()
+        return context
