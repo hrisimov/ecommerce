b193933530a0abc5ebf8dbad7e6bb0dc325aedc6
hrisimov <140465371+hrisimov@users.noreply.github.com>
2023-08-16 22:26:17 +0300

    Moved 'Address' model from 'accounts' to 'addresses' app

--- ecommerce/ecommerce/accounts/models.py
+++ ecommerce/ecommerce/accounts/models.py
@@ -1,5 +1,3 @@
-import uuid
-
 from django.contrib.auth import models as auth_models
 from django.contrib.auth import validators as auth_validators
 from django.core import validators
@@ -66,57 +64,3 @@ class Profile(models.Model):
         on_delete=models.CASCADE,
         primary_key=True,
     )
-
-
-class Address(models.Model):
-    PHONE_NUMBER_MAX_LENGTH = 20
-
-    PROVINCE_MAX_LENGTH = 50
-
-    TOWN_CITY_MAX_LENGTH = 50
-    TOWN_CITY_VERBOSE_NAME = 'Town/City'
-
-    ADDRESS_LINE_MAX_LENGTH = 150
-
-    id = models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4,
-        editable=False,
-    )
-
-    first_name = models.CharField(
-        max_length=Profile.FIRST_NAME_MAX_LENGTH,
-    )
-
-    last_name = models.CharField(
-        max_length=Profile.LAST_NAME_MAX_LENGTH,
-    )
-
-    phone_number = models.CharField(
-        max_length=PHONE_NUMBER_MAX_LENGTH,
-    )
-
-    province = models.CharField(
-        max_length=PROVINCE_MAX_LENGTH,
-    )
-
-    town_city = models.CharField(
-        max_length=TOWN_CITY_MAX_LENGTH,
-        verbose_name=TOWN_CITY_VERBOSE_NAME,
-    )
-
-    address_line = models.CharField(
-        max_length=ADDRESS_LINE_MAX_LENGTH,
-    )
-
-    default = models.BooleanField(
-        default=False,
-    )
-
-    user = models.ForeignKey(
-        to=EcommerceUser,
-        on_delete=models.CASCADE,
-    )
-
-    class Meta:
-        verbose_name_plural = 'Addresses'
--- ecommerce/ecommerce/addresses/models.py
+++ ecommerce/ecommerce/addresses/models.py
@@ -1,3 +1,63 @@
+import uuid
+
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+UserModel = get_user_model()
+
+
+class Address(models.Model):
+    FIRST_NAME_MAX_LENGTH = 30
+    LAST_NAME_MAX_LENGTH = 30
+
+    PHONE_NUMBER_MAX_LENGTH = 20
+
+    PROVINCE_MAX_LENGTH = 50
+
+    TOWN_CITY_MAX_LENGTH = 50
+    TOWN_CITY_VERBOSE_NAME = 'Town/City'
+
+    ADDRESS_LINE_MAX_LENGTH = 150
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+
+    first_name = models.CharField(
+        max_length=FIRST_NAME_MAX_LENGTH,
+    )
+
+    last_name = models.CharField(
+        max_length=LAST_NAME_MAX_LENGTH,
+    )
+
+    phone_number = models.CharField(
+        max_length=PHONE_NUMBER_MAX_LENGTH,
+    )
+
+    province = models.CharField(
+        max_length=PROVINCE_MAX_LENGTH,
+    )
+
+    town_city = models.CharField(
+        max_length=TOWN_CITY_MAX_LENGTH,
+        verbose_name=TOWN_CITY_VERBOSE_NAME,
+    )
+
+    address_line = models.CharField(
+        max_length=ADDRESS_LINE_MAX_LENGTH,
+    )
+
+    default = models.BooleanField(
+        default=False,
+    )
+
+    user = models.ForeignKey(
+        to=UserModel,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name_plural = 'Addresses'
