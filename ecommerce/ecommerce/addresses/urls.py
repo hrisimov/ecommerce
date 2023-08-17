3cfdd384c6e773169e25f974e607cbcb2eb1a6df
hrisimov <140465371+hrisimov@users.noreply.github.com>
2023-08-17 18:18:37 +0300

    Added 'address create' view

--- ecommerce/ecommerce/addresses/urls.py
+++ ecommerce/ecommerce/addresses/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+
+from ecommerce.addresses.views import AddressCreateView
+
 app_name = 'addresses'
 
-urlpatterns = ()
+urlpatterns = (
+    path('create/', AddressCreateView.as_view(), name='create'),
+)
--- ecommerce/ecommerce/addresses/views.py
+++ ecommerce/ecommerce/addresses/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import mixins as auth_mixins
+from django.views import generic as views
 
-# Create your views here.
+from ecommerce.addresses.forms import AddressCreateForm
+
+
+class AddressCreateView(auth_mixins.LoginRequiredMixin, views.CreateView):
+    form_class = AddressCreateForm
+    template_name = 'addresses/address_create.html'
+    # success_url = None
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
