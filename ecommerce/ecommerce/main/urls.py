022b0a8f11b7fea99bf80d2d6b42ae83cad5b972
hrisimov <140465371+hrisimov@users.noreply.github.com>
2023-08-19 06:10:49 +0300

    Added 'redirect' view

--- ecommerce/ecommerce/main/urls.py
+++ ecommerce/ecommerce/main/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.views.generic import RedirectView
 
 from ecommerce.main.views import DashboardView
 
 app_name = 'main'
 
 urlpatterns = (
+    path('', RedirectView.as_view(url=reverse_lazy('products:list'))),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
 )
